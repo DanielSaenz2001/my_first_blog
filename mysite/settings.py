2731438b358c212073cac113fb6202cafed86466
Daniel <daniel.saenz@upeu.edu.pe>
2019-02-25 11:04:30 -0500

    Probando la base de datos postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,13 +72,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'daniel',
+        'PASSWORD': '123321',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}'''
 
 
 # Password validation
